b1c56af80800c1640ee8cd5cf496b67ea6b406c8
Damir564 <ddamir.nizamutdinov@yandex.ru>
2023-04-07 12:55:16 +0300

    lab 3 almost completed

---
+++ lab3/main.js
@@ -0,0 +1,57 @@
+var modalForm = document.getElementById('modal')
+var registration = document.getElementById('registration')
+var close = document.getElementById('close')
+var showpassword = document.getElementById('showpassword')
+var password = document.getElementById('password')
+var email = document.getElementById('email')
+var passwordError = document.getElementById('password-error')
+var emailError = document.getElementById('email-error')
+var isFormValid = false;
+// password.setCustomValidity(`Пароль содержит меньше ${password.minLength} символов.\t`)
+// email.setCustomValidity(`Некорректная почта`)
+
+function validationCheck(event)
+{
+    const isValid = event.target.validity.valid;
+    if (event.target == password)
+    {
+        if (isValid)
+            password.setCustomValidity("")
+        else
+            password.setCustomValidity(`Пароль содержит меньше ${password.minLength} символов.\t`)
+        passwordError.textContent = password.validationMessage
+        
+    }
+    else if (event.target == email)
+    {
+        if (isValid)
+        email.setCustomValidity("")
+        else
+            email.setCustomValidity(`Некорректная почта`)
+        emailError.textContent = email.validationMessage
+    }
+    isFormValid = password.validity.valid * email.validity.valid
+    password.setCustomValidity("")
+    email.setCustomValidity("")
+}
+
+registration.onclick = () => {modal.style.display = 'block', registration.style.display = "none"}
+close.onclick = () => {registration.style.display = 'block', modal.style.display = "none"}
+modalForm.addEventListener('blur', (event) => validationCheck(event), true)
+// password.addEventListener('blur', (password) => validationCheck(password))
+// email.addEventListener('blur', (email) => validationCheck(email))
+showpassword.onpointerdown = () => password.type = 'text'
+showpassword.onpointerup = () => password.type = 'password'
+
+const formElement = document.getElementById('form-contact'); // извлекаем элемент формы
+formElement.addEventListener('submit', (e) => {
+  if (isFormValid){
+    e.preventDefault();
+    const formData = new FormData(formElement); // создаём объект FormData, передаём в него элемент формы
+    // теперь можно извлечь данные
+    const mail = formData.get('email'); 
+    const pass = formData.get('password');
+    console.log(mail, pass)
+    /* modal.style.display="none"; */
+  }
+});
