137bcbeb821daa14faef14d78680ac26d133a6ee
Damir564 <ddamir.nizamutdinov@yandex.ru>
2023-04-08 11:34:42 +0300

    lab 3 reafactored

--- lab3/main.js
+++ lab3/main.js
@@ -1,16 +1,14 @@
-var modalForm = document.getElementById("modal");
-var registration = document.getElementById("registration");
-var close = document.getElementById("close");
-var showpassword = document.getElementById("showpassword");
-var password = document.getElementById("password");
-var email = document.getElementById("email");
-var passwordError = document.getElementById("password-error");
-var emailError = document.getElementById("email-error");
+const modalForm = document.getElementById("modal");
+const registration = document.getElementById("registration");
+const close = document.getElementById("close");
+const showpassword = document.getElementById("showpassword");
+const password = document.getElementById("password");
+const email = document.getElementById("email");
+const passwordError = document.getElementById("password-error");
+const emailError = document.getElementById("email-error");
 const formElement = document.getElementById("form-contact");
 var isFormValid = false;
 var isModalShown = false;
-// password.setCustomValidity(`Пароль содержит меньше ${password.minLength} символов.\t`)
-// email.setCustomValidity(`Некорректная почта`)
 
 function validationCheck(event) {
   const isValid = event.target.validity.valid;
@@ -18,7 +16,7 @@ function validationCheck(event) {
     if (isValid) password.setCustomValidity("");
     else
       password.setCustomValidity(
-        `Пароль содержит меньше ${password.minLength} символов.\t`
+        `Пароль содержит меньше ${password.minLength} символов`
       );
     passwordError.textContent = password.validationMessage;
   } else if (event.target === email) {
@@ -26,7 +24,7 @@ function validationCheck(event) {
     else email.setCustomValidity(`Некорректная почта`);
     emailError.textContent = email.validationMessage;
   }
-  isFormValid = password.validity.valid * email.validity.valid;
+  isFormValid = password.validity.valid && email.validity.valid;
   password.setCustomValidity("");
   email.setCustomValidity("");
 }
@@ -42,17 +40,13 @@ function changeModalVisibility() {
   isModalShown = !isModalShown;
 }
 
-registration.onclick = () => changeModalVisibility();
-close.onclick = () => changeModalVisibility(); // {registration.style.display = 'block', modal.style.display = "none"}
-modalForm.addEventListener("blur", (event) => validationCheck(event), true);
-// password.addEventListener('blur', (password) => validationCheck(password))
-// email.addEventListener('blur', (email) => validationCheck(email))
+registration.onclick = changeModalVisibility;
+close.onclick = changeModalVisibility;
+modalForm.addEventListener("blur", validationCheck, true);
 showpassword.onpointerdown = () => (password.type = "text");
 showpassword.onpointerup = () => (password.type = "password");
 addEventListener("pointerdown", (event) => {
-  event.target !== modal &&
-  !modal.contains(event.target) &&
-  registration.style.display === "none"
+  event.target !== modal && !modal.contains(event.target) && isModalShown
     ? changeModalVisibility()
     : null;
 });
---
+++ lab4/main.js
@@ -0,0 +1,6 @@
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+import { default as Stats } from "https://cdnjs.cloudflare.com/ajax/libs/stats.js/r17/Stats.min.js";
+
+const clock = new THREE.Clock();
+let scene = new THREE.Scene();
+
