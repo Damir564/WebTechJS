66af2cc5bc014e99b3eea6c2f5b4dd07feafb98c
Damir564 <ddamir.nizamutdinov@yandex.ru>
2023-05-20 11:29:50 +0300

    Lab7 completed

---
+++ lab7/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const router = require('./router/router');
+
+const HOST = '127.0.0.1';
+const PORT = 5500;
+
+const app = express();
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+
+app.use("/", router);
+
+app.use((req, res) => {
+    res.status(404).send('Page not found!');
+});
+
+app.listen(PORT, HOST, () =>{
+    console.log(`Server running at http://${HOST}:${PORT}/`);
+})
